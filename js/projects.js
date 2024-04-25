482953dd272737d45c58acf3c95e6de5c5961e24
Raunak Tiwari <113747174+Raunak7t@users.noreply.github.com>
2024-04-25 22:00:26 +0530

    business logic started

---
+++ js/projects.js
@@ -0,0 +1,8 @@
+let projects = [
+  {
+    name: "Hello World!",
+    liveSrc: "Hello",
+    codeSrc: "",
+    desc: "Let's get started with ProjeX. You can now click on the project names from the aside menu and you will see the project preview. You can either get the live demo of project in a new tab screeen or you can download the code from github.",
+  },
+];
